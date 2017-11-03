ce08cfd3611479dd3bebf0320421635f2f3c5069
Richard Loera <richexp9@gmx.com>
2017-11-03 18:24:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,10 +7,12 @@ function happyHolidaysTo(name) {
   return `Happy holidays, ${name}!`
 }
 
-function happyHolidaysTo(holiday, name) {
+function happyHolidayTo(holiday, name) {
   return `Happy ${holiday}, ${name}!`
 }
 
 function holidayCountdown(holiday,days) {
   return `It's ${days} days until ${holiday}!`
 }
+
+
